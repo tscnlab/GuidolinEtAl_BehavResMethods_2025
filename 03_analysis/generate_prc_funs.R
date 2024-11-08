9d888a68404e99599c17d08034cee3f2c6edf5ca
Carolina Guidolin <127151898+caroguidish@users.noreply.github.com>
2024-11-08 12:22:14 +0100

    corrected code so that function works

--- 03_analysis/generate_prc_funs.R
+++ 03_analysis/generate_prc_funs.R
@@ -1,15 +1,16 @@
 # Main function to generate PRC metrics for specific parameters
-generate_prc_medi <- function(dataset, low_var, min_length, max_interrupt, medi_threshold) {
+generate_prc <- function(dataset, low_var, min_length, max_interrupt, threshold) {
   
   #Create a dynamic column names based on the variable from which we want to detect clusters
-  low_var_name <- paste0("low_", tolower(low_var))
-  cluster_name <- paste0(low_var_name, "_cluster")
+  low_var_name <- paste0("low_", tolower(low_var))  # e.g., "low_medi" or "low_pim"
+  cluster_name <- paste0(low_var_name, "_cluster")  # Expected column name to pass in data_find_clusters()
+  cluster_column <- paste0("is_", cluster_name)
   
-  medi_clusters <- dataset %>%
+  clusters <- dataset %>%
     ungroup() %>%
     mutate(State = if_else(State == "sleep", NA_character_, State)) %>% #converting all sleep values to NAs, since we do not want to use these for our classification 
     #Dinamically set the "low variable" flag, so that it can be either low_pim or low_medi
-    mutate(!!sym(low_var_name) := !!sym(low_var) < medi_threshold) %>%
+    mutate(!!sym(low_var_name) := !!sym(low_var) < threshold) %>%
     nest_by(Id) %>%
     mutate(
       data = list(
@@ -25,16 +26,16 @@ generate_prc_medi <- function(dataset, low_var, min_length, max_interrupt, medi_
     unnest(cols = data) %>%
     ungroup()
   
-  mediclusters_clean <- medi_clusters %>%
-    select(Id, Datetime, State, !!sym(cluster_name)) %>%
+  clusters_clean <- clusters %>%
+    select(Id, Datetime, State, !!sym(cluster_column)) %>%
     mutate(
       State = case_when(
         State == "on" ~ 1, 
         State == "off" ~ 0,
         is.na(State) ~ NA_real_), # keep sleep states as NA,
-      !!sym(cluster_name) := case_when(
-        !!sym(cluster_name) == TRUE ~ 0,
-        !!sym(cluster_name) == FALSE ~ 1
+      !!sym(cluster_column) := case_when(
+        !!sym(cluster_column) == TRUE ~ 0,
+        !!sym(cluster_column) == FALSE ~ 1
       )) 
   
   #We want to build a precision recall curve for our classifier algorithm, which, based o our "Ground truth", i.e. the wear log state values (on = 1 and off = 1), classifies the performance of our algorithm for detecting non-wear as follows:
@@ -43,15 +44,15 @@ generate_prc_medi <- function(dataset, low_var, min_length, max_interrupt, medi_
   #3) State = 1, cluster = 1 -> false positive
   #4) State = 1, cluster = 0 -> true negative
   
-  mediclusters_clean <- mediclusters_clean %>%
+  clusters_clean <- clusters_clean %>%
     mutate(classification = case_when(
-      State == 0 & !!sym(cluster_name) == 0 ~ "TP",
-      State == 0 & !!sym(cluster_name) == 1 ~ "FN",
-      State == 1 & !!sym(cluster_name) == 0 ~ "FP",
-      State == 1 & !!sym(cluster_name) == 1 ~ "TN",
+      State == 0 & !!sym(cluster_column) == 0 ~ "TP",
+      State == 0 & !!sym(cluster_column) == 1 ~ "FN",
+      State == 1 & !!sym(cluster_column) == 0 ~ "FP",
+      State == 1 & !!sym(cluster_column) == 1 ~ "TN",
       .default = NA_character_))
   
-  prc_medi <- mediclusters_clean %>%
+  prc <- clusters_clean %>%
     group_by(classification) %>% #!! if you want to visualise individual PRC, then you have to group_by(Id, classification). Else, only group_by(classification)
     summarise(count = n()) %>%
     pivot_wider(names_from = classification, values_from = count, values_fill = list(count=0)) %>%
@@ -60,5 +61,6 @@ generate_prc_medi <- function(dataset, low_var, min_length, max_interrupt, medi_
            PPV = TP/(TP+FP), #positive predictive value
            NPV = TN/(FN+TN)) #negative predictive value  
   
-  return(prc_medi)
+  return(prc)
 }
+
