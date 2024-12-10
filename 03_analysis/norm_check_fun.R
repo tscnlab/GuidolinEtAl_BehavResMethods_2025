8497e97668030a09ec8f2cc8ee552155c32a4ee3
Carolina Guidolin <127151898+caroguidish@users.noreply.github.com>
2024-12-10 11:12:33 +0100

    added normality check for deltas

--- 03_analysis/norm_check_fun.R
+++ 03_analysis/norm_check_fun.R
@@ -1,6 +1,7 @@
 # Function for testing normality 
 
-norm_check <- function (df) {
+## Testing normality on the means
+norm_check_means <- function (df) {
   
   df_qq <- df %>%
     mutate(mean_raw = as.numeric(mean_raw),
@@ -16,7 +17,27 @@ norm_check <- function (df) {
   
   ptot <- ggpubr::ggarrange(p1,p2,p3,
                     nrow = 1) 
-  annotated_plot <- ggpubr::annotate_figure(ptot, top = text_grob(paste0("Normality check: ", deparse(substitute(df)), " - raw, wrlg, clusters")))
+  annotated_plot <- ggpubr::annotate_figure(ptot, top = text_grob(paste0("Normality check means: ", deparse(substitute(df)), " - raw, wrlg, clusters")))
+  
+  return(annotated_plot)
+  
+}
+
+## Testing normality on the deltas
+norm_check_deltas <- function (df) {
+  
+  df_qq <- df %>%
+    mutate(delta_wrlg = as.numeric(delta_wrlg),
+           delta_clusters = as.numeric(delta_clusters))
+  
+  p1 <- ggpubr::ggqqplot(df_qq$delta_wrlg) +
+    theme(aspect.ratio = 1)
+  p2 <- ggpubr::ggqqplot(df_qq$delta_clusters) +
+    theme(aspect.ratio = 1)
+  
+  ptot <- ggpubr::ggarrange(p1,p2,
+                            nrow = 1) 
+  annotated_plot <- ggpubr::annotate_figure(ptot, top = text_grob(paste0("Normality check deltas: ", deparse(substitute(df)), " - delta_wrlg, delta_clusters")))
   
   return(annotated_plot)
   
