b6851fa9a5cb2755f7c095519030c2275798dbb0
Carolina Guidolin <carolina.guidolin@tuebingen.mpg.de>
2025-06-27 15:28:02 +0200

    Updated classification summary based on new alg parameters
    Min length of 21 changed performance

--- 01_import/import_wearlog.R
+++ 01_import/import_wearlog.R
@@ -1,8 +1,8 @@
-## ----setup, include=FALSE-----------------------------------------------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------------------------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
 
-## -----------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------------------------------------------------------
 filepath <- here::here("00_raw_data", "wearlog")
 
 # Get the files names from directory
@@ -10,7 +10,7 @@ wearlogfiles = list.files(filepath, pattern="*.csv", full.names = TRUE)
 
 
 
-## ----pressure, echo=FALSE-----------------------------------------------------------------------------------------
+## ----pressure, echo=FALSE-----------------------------------------------------------------------------------------------------------------
 wearlog_entries_raw <- 
   #filenames:
   wearlogfiles %>% 
@@ -23,5 +23,35 @@ wearlog_entries_raw <-
     State.encoding = 
       c("1", "0", "1", "0", "2", "2") #off coded as 0, on coded as 1, sleep coded as 2
     ) 
+
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------------
+wearlog_past <- wearlogfiles %>%
+  #import_Statechanges from LightLogR to create states based on timestamps
+  LightLogR::import_Statechanges(
+    sep = ";", dec = ",", Datetime.format = "dmyHM", tz = "Europe/Berlin", 
+    Id.colname = record_id,
+    State.colnames = 
+      c("wearlog_on",
+        "wearlog_off",
+        "wearlog_past_on",
+        "wearlog_past_off",
+        "wearlog_bed",
+        "wearlog_past_sleep"),
+    State.encoding = 
+      c("present",
+        "present",
+        "past",
+        "past",
+        "present",
+        "past")
+    ) 
+
+wearlog_past$State <- as.factor(wearlog_past$State)
+
+wearlog_past_summary <- wearlog_past %>%
+  group_by(State) %>%
+  summarise(n=n())
 
 
